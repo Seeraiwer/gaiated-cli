62654fefb03832c99aa3cee669b5e9f0b267dfee
Nichus <francois.leroy@gmx.fr>
2022-11-12 15:36:37 +0100

    modified:   README.md 	new file:   gaiaID2url.sh

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# gaiated-cli
-[OSINT] Exploitation of potential traces from the GaiaID/Google ID
+<div align="center"><h1>Gaiated-CLI</h1><div>
+    <img src="assets/img/os.svg" style="margin-right: 5px;"> 
+</div>
+<hr>
+<b>A script to exploit potential open traces of Google ID (GaiaID)</b>.
+</div>
---
+++ gaiaID2url.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+print_usage() {
+cat << EOF
+Usage : $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-s gaiaID]
+
+This script allows you to rotate on Google User ID/GaiaID and their possible traces.
+
+Options disponibles:
+
+-h, --help      Show this 
+-v, --verbose   Show debug informations
+-s, --selector  Select GaiaID"
+EOF
+  exit
+}
+
+parse_params() {
+  flag=0
+  param=''
+
+  while :; do
+    case "${1-}" in
+    -h | --help) print_usage ;;
+    -v | --verbose) set -x ;;
+    -s | --selector) 
+      userid="${2-}"
+      forge_url
+      ;;
+    -?*) manual_parse ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  args=("$@")
+
+  [[ -z "${param-}" ]] && manual_parse
+  [[ ${#args[@]} -eq 0 ]] && manual_parse
+
+  return 0
+}
+
+forge_url() {
+    if [[ ${#userid} -eq 21 ]] && [[ "$userid" =~ ^[0-9]+$ ]]; then
+            nohup xdg-open "https://google.com/maps/contrib/$userid" > /dev/null 2>&1
+            nohup xdg-open "https://web.archive.org/web/*/https://plus.google.com/*/$userid/*" > /dev/null 2>&1
+            nohup xdg-open "https://web.archive.org/web/*/https://get.google.com/albumarchive/$userid/*" > /dev/null 2>&1
+            nohup xdg-open "https://hangouts.google.com/?action=chat&pi=$userid"
+            echo "Alles Guet :)"
+            exit
+    else
+        echo "Ce n'est pas un Google ID (=> 21 caractères numériques)."
+    fi
+}
+
+manual_parse() {
+    echo "UserID Google : " 
+    read -r userid
+    forge_url
+}
+
+parse_params "$@"
